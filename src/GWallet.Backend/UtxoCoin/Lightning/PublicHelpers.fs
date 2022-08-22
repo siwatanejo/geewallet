8f80f0ee69afee6a022fdd9c2379bcec44d79d23
webwarrior <reg@webwarrior.ws>
2022-08-22 15:34:36 +0200

    WIP: work on routing
    Refactoring in QueryRoutingGossip - use DateTime instead of timestamps;
    began implementing routing graph creation/update

--- src/GWallet.Backend/UtxoCoin/Lightning/PublicHelpers.fs
+++ src/GWallet.Backend/UtxoCoin/Lightning/PublicHelpers.fs
@@ -53,7 +53,7 @@ module public Network =
 
     let public AcceptUpdateFee (lightningNode: NodeServer) = lightningNode.AcceptUpdateFee
 
-    let public QueryRoutingGossip (client: NodeClient) (firstTimestamp: uint32) (timestampRange: uint32) = 
+    let public QueryRoutingGossip (client: NodeClient) (timeRangeStart : DateTime) (timeRangeEnd : DateTime) = 
         let bfx_lnd0_node = 
             match client.ChannelStore.Currency with
             | Currency.BTC ->
@@ -61,7 +61,7 @@ module public Network =
                 NodeIdentifier.TcpEndPoint(NodeEndPoint.Parse Currency.BTC addr)
             | currency -> failwithf "Currency not supported: %A" currency
         async {
-            let! messages = client.QueryRoutingGossip bfx_lnd0_node firstTimestamp timestampRange
+            let! messages = client.QueryRoutingGossip bfx_lnd0_node timeRangeStart timeRangeEnd
             // for now return nothing, as IRoutingMsg is in assembly not referenced by frontend
             let arr = messages |> Seq.toArray
             ()
