54aa38e19cc0e5a9e7003754d63294e922b9d5b4
webwarrior <reg@webwarrior.ws>
2022-09-06 15:27:24 +0200

    WIP: attempt at serialization

--- src/GWallet.Backend/UtxoCoin/Lightning/RapidGossipSyncer.fs
+++ src/GWallet.Backend/UtxoCoin/Lightning/RapidGossipSyncer.fs
@@ -35,7 +35,7 @@ module RapidGossipSyncer =
             HtlcMaximumMSat: uint64
         }
 
-    type private ChannelUpdates =
+    type internal ChannelUpdates =
         {
             Forward: UnsignedChannelUpdateMsg option
             Backward: UnsignedChannelUpdateMsg option
@@ -53,9 +53,119 @@ module RapidGossipSyncer =
                     match self.Backward with
                     | Some(prevUpd) when update.Timestamp < prevUpd.Timestamp -> self
                     | _ -> { self with Backward = Some(update) }
+    
+    let private deserialize<'T when 'T: (new: unit -> 'T) and 'T :> ILightningSerializable<'T>> (stream: LightningReaderStream) =
+        let inst = new 'T()
+        inst.Deserialize stream
+        inst
+
+    type internal GossipData =
+        {
+            Announcements: CompactAnnouncment[]
+            Updates: Map<ShortChannelId, ChannelUpdates>
+        }
+        with
+            member self.Serialize(stream: System.IO.Stream) =
+                use lightningStream = new DotNetLightning.Serialization.LightningWriterStream(stream)
+
+                lightningStream.Write(self.Announcements.Length, false)
+                for ann in self.Announcements do
+                    self.SerializeAnnouncement(ann, lightningStream)
+                
+                lightningStream.Write(self.Updates.Count, false)
+                for pair in self.Updates do
+                    self.SerializeUpdatesPair(pair, lightningStream)
+
+            member self.SerializeAnnouncement(ann, lightningStream) =
+                lightningStream.Write(ann.ShortChannelId.ToUInt64(), false)
+                lightningStream.Write ann.NodeId1.Value
+                lightningStream.Write ann.NodeId2.Value
+                match ann.ChannelFeatures with
+                | Ok(features) -> 
+                    lightningStream.Write 0uy
+                    let byteArray = features.ToByteArray()
+                    lightningStream.Write(uint8 byteArray.Length)
+                    lightningStream.Write(byteArray)
+                | Error(FeatureError.BogusFeatureDependency(_)) ->
+                    lightningStream.Write 1uy
+                | Error(FeatureError.UnknownRequiredFeature(_)) ->
+                    lightningStream.Write 2uy
+
+            member self.SerializeUpdatesPair(pair, lightningStream) =
+                lightningStream.Write(pair.Key.ToUInt64(), false)
+                let existenceFlags = 
+                    (if pair.Value.Forward.IsSome then 0b01uy else 0uy) 
+                    ||| (if pair.Value.Backward.IsSome then 0b10uy else 0uy)
+                assert(existenceFlags <= 0b11uy)
+                lightningStream.Write existenceFlags
+                pair.Value.Forward |> Option.iter (fun upd -> (upd :> ILightningSerializable<_>).Serialize lightningStream)
+                pair.Value.Backward |> Option.iter (fun upd -> (upd :> ILightningSerializable<_>).Serialize lightningStream)
+
+            static member Deserialize(stream: System.IO.Stream) : GossipData =
+                use lightningStream = new DotNetLightning.Serialization.LightningReaderStream(stream)
+                
+                let annCount = lightningStream.ReadInt32 false
+                let announcements = 
+                    Array.init
+                        annCount
+                        (fun _ -> GossipData.DeserializeAnnouncement lightningStream)
+                                
+                let updatesCount = lightningStream.ReadInt32 false
+                let updatePairs =
+                    Array.init
+                        updatesCount
+                        (fun _ -> GossipData.DeserializeUpdatePair lightningStream)
+
+                {
+                    Announcements = announcements
+                    Updates = updatePairs |> Map.ofArray
+                }
+
+            static member DeserializeAnnouncement(lightningStream) = 
+                let channelId = lightningStream.ReadUInt64(false)
+                let nodeId1 = NodeId(lightningStream.ReadPubKey())
+                let nodeId2 = NodeId(lightningStream.ReadPubKey())
+                let featuresFlag = lightningStream.ReadByte()
+                let featureBits =
+                    match featuresFlag with
+                    | 0uy -> 
+                        let count = lightningStream.ReadByte()
+                        let byteArray = lightningStream.ReadBytes(int count)
+                        FeatureBits.TryCreate byteArray
+                    | 1uy ->
+                        Error(FeatureError.BogusFeatureDependency(""))
+                    | 2uy ->
+                        Error(FeatureError.UnknownRequiredFeature(""))
+                    | _ -> 
+                        failwith ("unexpected value: " + string(featuresFlag))
+                {
+                    ChannelFeatures = featureBits
+                    ShortChannelId = channelId |> ShortChannelId.FromUInt64
+                    NodeId1 = nodeId1
+                    NodeId2 = nodeId2
+                }
+
+            static member DeserializeUpdatePair(lightningStream) = 
+                let key = lightningStream.ReadUInt64(false) |> ShortChannelId.FromUInt64
+                
+                let existenceFlags = lightningStream.ReadByte()
+                let value =
+                    match existenceFlags with
+                    | 0b00uy -> 
+                        ChannelUpdates.Empty
+                    | 0b01uy ->
+                        { Forward= Some(deserialize<UnsignedChannelUpdateMsg> lightningStream); Backward = None }
+                    | 0b10uy ->
+                        { Forward = None; Backward = Some(deserialize<UnsignedChannelUpdateMsg> lightningStream) }
+                    | 0b11uy ->
+                        { Forward = Some(deserialize<UnsignedChannelUpdateMsg> lightningStream);
+                          Backward = Some(deserialize<UnsignedChannelUpdateMsg> lightningStream) }
+                    | _ -> failwith ("Unexpected value: " + (string existenceFlags))
+
+                key, value
 
     /// see https://github.com/lightningdevkit/rust-lightning/tree/main/lightning-rapid-gossip-sync/#custom-channel-update
-    module CustomChannelUpdateFlags =
+    module internal CustomChannelUpdateFlags =
         let direction = 1uy
         let disableCHannel = 2uy
         let htlcMaximumMsat = 4uy
@@ -178,7 +288,7 @@ module RapidGossipSyncer =
                             Timestamp = backdatedTimestamp
                             ChainHash = Network.Main.GenesisHash
                             ChannelFlags = standardChannelFlag
-                            MessageFlags = 0uy
+                            MessageFlags = 1uy
                             CLTVExpiryDelta = cltvExpiryDelta |> BlockHeightOffset16
                             HTLCMinimumMSat = htlcMinimumMSat |> LNMoney.MilliSatoshis
                             FeeBaseMSat = feeBaseMSat |> LNMoney.MilliSatoshis
@@ -196,6 +306,14 @@ module RapidGossipSyncer =
                     readUpdates (remainingCount - 1u) shortChannelId newUpdates
 
             let updates = readUpdates updatesCount 0UL Map.empty
+
+            let gossipData = { Announcements = announcements; Updates = updates }
+
+            // serialization roundtrip test
+            use ms = new System.IO.MemoryStream()
+            use rs = new System.IO.MemoryStream(ms.ToArray())
+            let deserializedData = GossipData.Deserialize rs
+            assert(gossipData = deserializedData)
 
             return ()
         }
