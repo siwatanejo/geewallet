dde7b52ec459fbf102f8819fb16887740a0d92cd
webwarrior <reg@webwarrior.ws>
2023-01-05 14:10:31 +0100

    CI: upgrade bitcoin and lnd versions
    Upgrade bitcoin to version 22.0 as lnd is built against it
    starting from version 14.0.
    Upgrade lnd to v0.15.0-beta.

--- scripts/install_regtest_dependencies.sh
+++ scripts/install_regtest_dependencies.sh
@@ -9,8 +9,8 @@ export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
 
 # Install bitcoin core
 sudo apt install -y curl tar gzip
-curl -OJL https://bitcoin.org/bin/bitcoin-core-0.20.0/bitcoin-0.20.0-x86_64-linux-gnu.tar.gz
-sudo tar -C /usr/local --strip-components 1 -xzf bitcoin-0.20.0-x86_64-linux-gnu.tar.gz
+curl -OJL https://bitcoin.org/bin/bitcoin-core-22.0/bitcoin-22.0-x86_64-linux-gnu.tar.gz
+sudo tar -C /usr/local --strip-components 1 -xzf bitcoin-22.0-x86_64-linux-gnu.tar.gz
 
 # Install electrs
 sudo DEBIAN_FRONTEND="noninteractive" apt install -y clang cmake build-essential cargo
@@ -29,7 +29,7 @@ sudo tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
 sudo apt install -y curl tar gzip unzip make git
 git clone https://github.com/lightningnetwork/lnd
 cd lnd
-git checkout v0.14.1-beta
+git checkout v0.15.0-beta
 make
 make install
 cd ..
