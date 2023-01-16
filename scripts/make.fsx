0b4629fa5b93ef1df8a39bb094a7357c68947785
Parham Saremi <parhaamsaremi@gmail.com>
2023-01-16 15:41:17 +0330

    Frontend.Maui: build Maui on linux using GTK
    We use our own fork of maui for building maui-gtk since we
    fixed some things.
    
    Maui: added as submodule.
    Gtk workload: as automatic workload manifest detection doesn't
    work for some reason, download and uzip mainfest file so .NET
    knows about gtk workload.
    
    Because of new versions of manifest, we got the following
    error. So by skipping manifest update we make sure that
    we don't get unwanted updates.
    ```
    Workload installation failed: Failed to install manifest gtksharp.net.sdk.gtk version 3.24.24-rev.87-develop: gtksharp.net.sdk.gtk.manifest-6.0.300::3.24.24-rev.87-develop is not found in NuGet feeds https://pkgs.dev.azure.com/dnceng/public/_packaging/darc-pub-dotnet-runtime-531f715f/nuget/v3/index.json;https://pkgs.dev.azure.com/dnceng/public/_packaging/darc-pub-dotnet-emsdk-3f6c45a2/nuget/v3/index.json;https://pkgs.dev.azure.com/dnceng/public/_packaging/darc-pub-dotnet-emsdk-3f6c45a2-1/nuget/v3/index.json;https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-eng/nuget/v3/index.json;https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-public/nuget/v3/index.json;https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet6/nuget/v3/index.json;https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet7/nuget/v3/index.json;https://pkgs.dev.azure.com/xamarin/public/_packaging/SkiaSharp/nuget/v3/index.json;https://nuget.pkg.github.com/GtkSharp/index.json"..
    ```
    
    Co-authored-by: webwarrior <reg@webwarrior.ws>

--- scripts/make.fsx
+++ scripts/make.fsx
@@ -459,6 +459,7 @@ let MakeAll (maybeConstant: Option<string>) =
 #endif
     let buildConfig = BinaryConfig.Debug
     let frontend,_ = JustBuild buildConfig maybeConstant
+    CopyXamlFiles()
     frontend,buildConfig
 
 let RunFrontend (frontend: Frontend) (buildConfig: BinaryConfig) (maybeArgs: Option<string>) =
