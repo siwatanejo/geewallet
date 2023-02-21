efc3389e267cea8b64ed82e96ddc71997ba2dea0
webwarrior <reg@webwarrior.ws>
2023-02-21 14:11:53 +0100

    Frontend.Maui: set line break mode in labels
    Add mapping to Label mapper for Gtk to properly set line break
    mode in labels.
    Fixes problem with words being split using hyphen (-).

--- src/GWallet.Frontend.Maui/MauiProgram.fs
+++ src/GWallet.Frontend.Maui/MauiProgram.fs
@@ -5,6 +5,7 @@ open Gdk
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.DependencyInjection.Extensions
+open Microsoft.Maui.Graphics.Platform.Gtk
 #endif
 open Microsoft.Maui
 open Microsoft.Maui.Controls
@@ -13,19 +14,36 @@ open Microsoft.Maui.Controls.Compatibility.Hosting
 open Microsoft.Maui.Controls.Hosting
 open Microsoft.Maui.Hosting
 open Microsoft.Maui.LifecycleEvents
+open Microsoft.Maui.Handlers
 
 type MauiProgram =
     static member CreateMauiApp() =
-        MauiApp
-            .CreateBuilder()
-            .UseMauiApp<App>()
+        let app =
+            MauiApp
+                .CreateBuilder()
+                .UseMauiApp<App>()
 #if GTK 
-            .UseMauiCompatibility()
+                .UseMauiCompatibility()
 #endif
-            .ConfigureFonts(fun fonts ->
-                fonts
-                    .AddFont("OpenSans-Regular.ttf", "OpenSansRegular")
-                    .AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold")
-                |> ignore
-            )
-            .Build()
+                .ConfigureFonts(fun fonts ->
+                    fonts
+                        .AddFont("OpenSans-Regular.ttf", "OpenSansRegular")
+                        .AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold")
+                    |> ignore
+                )
+                .Build()
+#if GTK        
+        Handlers.LabelHandler.Mapper.PrependToMapping(
+            "Label.LineBreakMode",
+            fun (handler: ILabelHandler) (view: ILabel) ->
+                let lineBreakMode = (view :?> Label).LineBreakMode
+                handler.PlatformView.LineWrapMode <-
+                    match lineBreakMode with
+                    | LineBreakMode.CharacterWrap -> Pango.WrapMode.Char
+                    | _ -> Pango.WrapMode.Word
+                handler.PlatformView.LineWrap <-
+                    match lineBreakMode with
+                    | LineBreakMode.NoWrap -> false
+                    | _ -> true )
+#endif        
+        app
