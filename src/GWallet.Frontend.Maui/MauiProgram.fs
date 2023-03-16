f795ddc37b4a902927e90ac8a92079cfc08468f4
Parham <parhaamsaremi@gmail.com>
2023-03-16 16:44:04 +0330

    Frontend.Maui: added LoadingPage
    Added LoadingPage.
    
    Abstracted StartTimer function to work on both Xamarin.Forms and Maui
    (Device.StartTimer is deprecated in Maui).
    
    Add mappers for BorderColor and CornerRadius for Frame elements so they
    can now have borders.
    
    Co-authored-by: Parham <parhaamsaremi@gmail.com>

--- src/GWallet.Frontend.Maui/MauiProgram.fs
+++ src/GWallet.Frontend.Maui/MauiProgram.fs
@@ -5,17 +5,40 @@ open Gdk
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.DependencyInjection.Extensions
+open Microsoft.Maui.Graphics.Platform.Gtk
+open GWallet.Backend.FSharpUtil.ReflectionlessPrint
 #endif
 open Microsoft.Maui
 open Microsoft.Maui.Controls
-open Microsoft.Maui.Controls.Compatibility
 open Microsoft.Maui.Controls.Compatibility.Hosting
 open Microsoft.Maui.Controls.Hosting
+open Microsoft.Maui.Handlers
 open Microsoft.Maui.Hosting
-open Microsoft.Maui.LifecycleEvents
 
 type MauiProgram =
     static member CreateMauiApp() =
+#if GTK
+        ContentViewHandler.ViewMapper.AppendToMapping(
+             "BorderColor",
+             fun handler view ->
+                 match view with
+                 | :? Frame as frame ->
+                    let contentView = handler.PlatformView :?> Microsoft.Maui.Platform.ContentView
+                    let colorString = frame.BorderColor.ToGdkRgba().ToString()
+                    let mainNode = contentView.CssMainNode()
+                    contentView.SetStyleValueNode(SPrintF1 "1px solid %s" colorString, mainNode, "border")
+                 | _ -> ())
+        
+        ContentViewHandler.ViewMapper.AppendToMapping(
+             "CornerRadius",
+             fun handler view ->
+                 match view with
+                 | :? Frame as frame ->
+                    let contentView = handler.PlatformView :?> Microsoft.Maui.Platform.ContentView
+                    let mainNode = contentView.CssMainNode()
+                    contentView.SetStyleValueNode(SPrintF1 "%fpx" frame.CornerRadius, mainNode, "border-radius")
+                 | _ -> ())
+#endif        
         MauiApp
             .CreateBuilder()
             .UseMauiApp<App>()
